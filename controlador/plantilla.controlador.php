c39e0bab3a3276a270f46a9ecb0315e381e646d1
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2021-03-29 15:23:27 -0600

    Update plantilla.controlador.php

--- controlador/plantilla.controlador.php
+++ controlador/plantilla.controlador.php
@@ -7,7 +7,7 @@ class controladorPlantilla{
     }
 
     public static function ruta(){
-        return 'http://localhost:8888/GitHub/CODERHOUSEJS/PROYECTO-CODERHOUSE/';
+        return 'http://localhost:8888/GitHub/RichOmaar.github.io/';
     }
 }
 
